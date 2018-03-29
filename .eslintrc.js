9bc232297ba24a0a6979af96d3a3529b30317f66
Jeff Posnick <jeffy@google.com>
2018-03-29 17:18:56 -0400

    WIP.

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['eslint:recommended', 'google'],
+  env: {
+    serviceworker: true,
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  rules: {
+    'require-jsdoc': 0,
+  }
+};
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,14 @@
-const {html} = require('common-tags');
-const fetch = require('node-fetch');
 const fse = require('fs-extra');
 const functions = require('firebase-functions');
 const path = require('path');
+
+const makeRequest = require('../lib/make-request');
 
 let partialCache;
 async function loadPartials() {
   if (!partialCache) {
     const readFilePromises = ['head.html', 'sidebar.html', 'foot.html']
-      .map((name) => path.join('..', 'public', 'partials', name))
+      .map((name) => path.join('..', 'www', 'partials', name))
       .map((filePath) => fse.readFile(filePath, 'utf-8'));
     const [head, sidebar, foot] = await Promise.all(readFilePromises);
     partialCache = {head, sidebar, foot};
@@ -17,18 +17,16 @@ async function loadPartials() {
   return partialCache;
 }
 
-async function requestIndex() {
-  const url = 'https://api.stackexchange.com/2.2/questions/featured?order=desc&sort=activity&site=stackoverflow';
-  const response = await fetch(url);
-  const json = await response.json();
-  return json.items.map((item) => {
-    return html`<p><a href="${item.link}">${item.title}</a></p>`;
-  }).join('');
-}
-
 module.exports.index = functions.https.onRequest(async (request, response) => {
   const partials = await loadPartials();
-  const body = await requestIndex();
+  const body = await makeRequest.index();
+  const html = partials.head + partials.sidebar + body + partials.foot;
+  response.status(200).send(html);
+});
+
+module.exports.question = functions.https.onRequest(async (request, response) => {
+  const partials = await loadPartials();
+  const body = await makeRequest.question(request.url.split('/').pop());
   const html = partials.head + partials.sidebar + body + partials.foot;
   response.status(200).send(html);
 });
