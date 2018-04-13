496fdc7b84a5ad4afd23858a090fb6d8280a70d6
Jeff Posnick <jeffy@google.com>
2018-04-13 17:05:49 -0400

    Escaping.

--- src/urls.mjs
+++ src/urls.mjs
@@ -2,10 +2,10 @@ const PREFIX = `https://api.stackexchange.com/2.2`;
 
 export function listQuestionsForTag(tag) {
   return `${PREFIX}/questions?pagesize=100&order=desc&sort=votes&tagged=` +
-    `${tag}&site=stackoverflow&filter=!C(o*VY))7BGSrm5xK`;
+    `${encodeURIComponent(tag)}&site=stackoverflow&filter=!C(o*VY))7BGSrm5xK`;
 }
 
 export function getQuestion(questionId) {
-  return `${PREFIX}/questions/${questionId}?site=stackoverflow&filter=` +
-    `!E-NoEOOqk.KxiVtgwUSr(q72V0fqfidE4Y)th*`;
+  return `${PREFIX}/questions/${encodeURIComponent(questionId)}?site=` +
+    `stackoverflow&filter=!E-NoEOOqk.KxiVtgwUSr(q72V0fqfidE4Y)th*`;
 }
